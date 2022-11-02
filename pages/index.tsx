95a05a8856149f9c22340923109aeda43874f050
khiêm bá <bakhjem@gmail.com>
2022-11-02 16:22:24 +0700

    fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,24 @@ import { getNovelUpdate } from "../API/APIManage";
 import { Layout } from "../components/PC/Layout";
 import Post from "../interfaces/post";
 import { CMS_NAME } from "../lib/constants";
+const shimmer = (w: number, h: number) => `
+<svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+  <defs>
+    <linearGradient id="g">
+      <stop stop-color="#333" offset="20%" />
+      <stop stop-color="#222" offset="50%" />
+      <stop stop-color="#333" offset="70%" />
+    </linearGradient>
+  </defs>
+  <rect width="${w}" height="${h}" fill="#333" />
+  <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
+  <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
+</svg>`
 
+const toBase64 = (str: string) =>
+  typeof window === 'undefined'
+    ? Buffer.from(str).toString('base64')
+    : window.btoa(str)
 type Props = {
   allPosts: Post[];
 };
@@ -44,49 +61,41 @@ export default function Index() {
           <Grid.Container gap={1} justify="space-between">
             {datanovel?.slice(0, 18).map((item, index) => (
               <Grid xs={6} sm={2} key={index}>
-                <Card isHoverable isPressable>
-                  <Card.Body css={{ p: 0 }}>
-                    <div
-                      style={{
-                        width: '100%',
-                        height: 210,
-                        position: "relative",
+                <div className="max-w-sm rounded-lg border border-gray-200 shadow-md">
+                  <Image
+                    src={item.cover}
+                    className="px-2 "
+                    placeholder="blur"
+                    width={250}
+                    height={210}
+                    alt={"ddv"}
+                    style={{
+                      maxWidth: "100%",
+                      maxHeight:210,
+                      height: "auto",
+                      objectFit: "cover",
+                    }}
+                    blurDataURL={`data:image/svg+xml;base64,${toBase64(shimmer(700, 475))}`}
+                    priority
+                  />
+                  <div className="p-5">
+                    <Text h2 css={{
+                        // color: "$black",
+                        fontWeight: "$semibold",
+                      }} className="font-medium text-left">
+                      {item.novelsname}
+                    </Text>
+                    <Text
+                      css={{
+                        color: "$accents7",
+                        fontWeight: "$semibold",
+                        fontSize: "$sm",
                       }}
                     >
-                      <Image
-                        src={item.cover}
-                        className="px-2 "
-                        fill
-                        sizes="100vw"
-                        alt={"ddv"}
-                        style={{
-                          objectFit: 'contain',
-                        }}
-                      />
-                    </div>
-                    {/* <Image
-                      src={item.cover}
-                      objectFit=""
-                      // width="100%"
-                      // height={210}
-                      alt={item.novelsname}
-                    /> */}
-                    <div className="items-start flex flex-col px-2 py-2">
-                      <Text h2 className="font-medium text-left">
-                        {item.novelsname}
-                      </Text>
-                      <Text
-                        css={{
-                          color: "$accents7",
-                          fontWeight: "$semibold",
-                          fontSize: "$sm",
-                        }}
-                      >
-                        {item.lasterchapter}
-                      </Text>
-                    </div>
-                  </Card.Body>
-                </Card>
+                      {item.lasterchapter}
+                    </Text>
+                  </div>
+                </div>
               </Grid>
             ))}
           </Grid.Container>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
         'accent-1': '#FAFAFA',
         'accent-2': '#EAEAEA',
         'accent-7': '#333',
+        'black': '#000',
         success: '#0070f3',
         cyan: '#79FFE1',
       },
