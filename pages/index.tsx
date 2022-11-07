739a886bb0439457d4dc8e60bcbb5c5df1959680
khiêm bá <bakhjem@gmail.com>
2022-11-07 17:22:40 +0700

    add

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,6 @@
-import { Card, Container, Grid, Link, Text } from "@nextui-org/react";
+import { Skeleton } from "@mantine/core";
+import { Link, Text } from "@nextui-org/react";
+import { NextSeo } from "next-seo";
 import Head from "next/head";
 import Image from "next/image";
 import { useEffect, useState } from "react";
@@ -27,12 +29,15 @@ const toBase64 = (str: string) =>
 type Props = {
   allPosts: Post[];
 };
-
+const dataloading = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
 export default function Index() {
+  const [isLoading, setIsLoading] = useState(false);
   const [datanovel, setDatanovel] = useState([]);
   useEffect(() => {
+    setIsLoading(true);
     getNovelUpdate({ page: 1 })
       .then((res: any) => {
+        setIsLoading(false);
         setDatanovel(res?.data);
       })
       .catch((e) => {});
@@ -41,9 +46,10 @@ export default function Index() {
   return (
     <>
       <Layout>
-        <Head>
-          <title>Next.js Blog Example with {CMS_NAME}</title>
-        </Head>
+      <NextSeo
+      title="NovelWorld | Read Novels online free"
+      description="NovelWorld is the foremost English publisher of Chinese and Korean webnovels and light novels"
+    />
         <div className="container mx-auto">
           <div className="w-full flex justify-between">
             <Text
@@ -58,55 +64,65 @@ export default function Index() {
             </Text>
             <Link href="#">MORE</Link>
           </div>
-          <div className="grid grid-cols-6 gap-2">
-            {datanovel?.slice(0, 18).map((item, index) => (
-              <Link href={`/novel/${item.idnovel}`}>
-                <a
-                  key={index}
-                  className="max-w-sm rounded-lg h-full border border-gray-200 shadow-md"
-                >
-                  <Image
-                    src={item.cover}
-                    className="px-2 "
-                    placeholder="blur"
-                    width={250}
-                    height={210}
-                    alt={"ddv"}
-                    style={{
-                      maxWidth: "100%",
-                      maxHeight: 210,
-                      height: "auto",
-                      objectFit: "cover",
-                    }}
-                    blurDataURL={`data:image/svg+xml;base64,${toBase64(
-                      shimmer(700, 475)
-                    )}`}
-                    priority
-                  />
-                  <div className="p-5">
-                    <Text
-                      h2
-                      css={{
-                        // color: "$black",
-                        fontWeight: "$semibold",
-                      }}
-                      className="font-medium text-left"
-                    >
-                      {item.novelsname}
-                    </Text>
-                    <Text
-                      css={{
-                        color: "$accents7",
-                        fontWeight: "$semibold",
-                        fontSize: "$sm",
-                      }}
-                    >
-                      {item.lasterchapter}
-                    </Text>
+          <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-2">
+            {isLoading
+              ? dataloading.map((item, index) => (
+                  <div
+                    key={index}
+                    className="max-w-sm rounded-lg h-full border border-gray-200 shadow-md"
+                  >
+                    <Skeleton height={210} mb="xl" />
+                    <Skeleton height={18} radius="xl" mb="xl" />
                   </div>
-                </a>
-              </Link>
-            ))}
+                ))
+              : datanovel?.slice(0, 18).map((item, index) => (
+                  <Link href={`/novel/${item.idnovel}`}>
+                    <a
+                      key={index}
+                      className="max-w-sm rounded-lg h-full border border-gray-200 shadow-md"
+                    >
+                      <Image
+                        src={item.cover}
+                        className="px-2 "
+                        placeholder="blur"
+                        width={250}
+                        height={210}
+                        alt={"ddv"}
+                        style={{
+                          maxWidth: "100%",
+                          maxHeight: 210,
+                          height: "auto",
+                          objectFit: "cover",
+                        }}
+                        blurDataURL={`data:image/svg+xml;base64,${toBase64(
+                          shimmer(700, 475)
+                        )}`}
+                        priority
+                      />
+                      <div className="p-5">
+                        <Text
+                          h2
+                          css={{
+                            // color: "$black",
+                            fontWeight: "$semibold",
+                          }}
+                          className="font-medium text-left"
+                        >
+                          {item.novelsname}
+                        </Text>
+                        <Text
+                          css={{
+                            color: "$accents7",
+                            fontWeight: "$semibold",
+                            fontSize: "$sm",
+                          }}
+                        >
+                          {item.lasterchapter}
+                        </Text>
+                      </div>
+                    </a>
+                  </Link>
+                ))}
           </div>
         </div>
       </Layout>
